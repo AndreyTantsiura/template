f4a407b2f642e95fc91b46d902513c0aaca1d20b
Andrew <https://github.com/AndreyTantsiura>
2024-01-14 16:20:15 +0200

    fix: fixed styles HeaderLayout

--- src/app/layouts/HeaderLayout/HeaderLayout.jsx
+++ src/app/layouts/HeaderLayout/HeaderLayout.jsx
@@ -2,12 +2,14 @@
 import { Outlet } from 'react-router-dom';
 //widgets
 import { Header } from '@/widgets/header';
+//styles
+import styles from './HeaderLayout.module.scss';
 
 export const HeaderLayout = ({}) => {
   return (
-    <>
+    <dic className={styles.HeaderLayout}>
       <Header />
       <Outlet />
-    </>
+    </dic>
   );
 };
